5b2f5b32bb92a0bc28da11bb86c810458f8b1a76
Nikolaos Passalis <passalis@auth.gr>
2025-01-27 12:00:07 +0200

    Version updates.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,7 @@
 
 ## Version 0.0.1
 - Library concept ready.
+- 
+## Version 0.0.2
+- 7 first datasets added.
+- Initial version of the API ready.
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'chelo'
 copyright = '2025, Nikolaos Passalis'
 author = 'Nikolaos Passalis'
-release = '0.0.1'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
