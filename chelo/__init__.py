597247dffd4a2bdbbfc28adbc74d62383ee79dcd
Nikolaos Passalis <passalis@users.noreply.github.com>
2025-05-15 07:41:16 +0300

    Fixed module discovery issue (#16)
    * Fixed module discovery issue
    
    * Update pyproject.toml
    
    * Update conf.py

--- chelo/__init__.py
+++ chelo/__init__.py
@@ -6,4 +6,5 @@ import importlib
 
 __all__ = ["CheLoDataset", "DatasetRegistry",]
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    importlib.import_module(f".{module_name}", package=__name__)
+    if 'chelo' in loader.path:
+        importlib.import_module(f".{module_name}", package=__name__)
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'chelo'
 copyright = '2025, Nikolaos Passalis'
 author = 'Nikolaos Passalis'
-release = '0.0.4'
+release = '0.0.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
